fde0334500cb5dc3bd3cca27e1dbb869f4bf25de
Andre <anlivoliveira@gmail.com>
2025-11-23 09:33:20 -0300

    feat: add debug and verbose to BasicTransform

--- src/datapipeline/processing/basic_transform.py
+++ src/datapipeline/processing/basic_transform.py
@@ -1,13 +1,30 @@
 import pandas as pd
 from ..core.interfaces import Transformer
 from typing import Any
+from rich import print
 
 
 
 class BasicTransform(Transformer):
+    def __init__(self, debug: bool = False, verbose: bool = False) -> None:
+        self.debug = debug
+        self.verbose = verbose
+    
     def transform(self, data: Any) -> Any:
         
-        # Example transformation: Fill missing values and convert temperature to Fahrenheit
+
         df: pd.DataFrame = pd.DataFrame(data)
+        
         df.columns = [c.strip().lower() for c in df.columns]
+        
+        if self.debug:
+            print(f"[yellow]Data after transforming name columns to lowercase and stripping whitespace:[/yellow] {df.columns.to_list()}")
+            
+        
+        if self.verbose:
+            print(f"[green]Returning df after transformer[/green]")
+        
         return df
+
+        
+        
